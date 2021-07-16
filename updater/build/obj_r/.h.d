767bd83740a9c64237b85c51a24f064d4b925373
Maurizio <mdegirolami@gmail.com>
2021-07-16 16:11:03 +0200

    updater updates c_cpp_properties

--- updater/build/obj_r/.h.d
+++ updater/build/obj_r/.h.d
@@ -1,3 +1,5 @@
 .h: c:/Users/degimau/.vscode/extensions/mauriziodegirolami.vscode-sing-0.40.0/sdk/sing_headers/sio.sing \
+ c:/Users/degimau/.vscode/extensions/mauriziodegirolami.vscode-sing-0.40.0/sdk/sing_headers/sys.sing \
  c:/Users/degimau/.vscode/extensions/mauriziodegirolami.vscode-sing-0.40.0/sdk/sing_headers/str.sing \
- c:/Users/degimau/.vscode/extensions/mauriziodegirolami.vscode-sing-0.40.0/sdk/sing_headers/sort.sing
+ c:/Users/degimau/.vscode/extensions/mauriziodegirolami.vscode-sing-0.40.0/sdk/sing_headers/json.sing \
+ d:/Documents/w12/GitHub/stay/updater/src/buildupd.sing
